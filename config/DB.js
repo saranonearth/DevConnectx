f0f6bae12687891f5931aa6992ae6c457b11c1ce
saranonearth <saran.suresh@outlook.com>
2019-06-16 06:03:11 +0530

    User Registration and Login Route

---
+++ config/DB.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const config = require('config');
+
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true
+    });
+
+    console.log('MongoDB Connected bro!');
+  } catch (err) {
+    console.log(err.message);
+    //Exit if the connection of DB fails
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ models/User.js
@@ -0,0 +1,28 @@
+//Mongoose Schema for Users
+
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    rquired: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  avatar: {
+    type: String
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = User = mongoose.model('user', UserSchema);
